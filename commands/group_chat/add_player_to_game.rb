5c1255326c3320fc574b8746f1764271df35ffbe
Madjid Boudis <badido18@live.fr>
2023-01-22 15:02:35 +0100

    fix : confirm pending player on addme

--- commands/group_chat/add_player_to_game.rb
+++ commands/group_chat/add_player_to_game.rb
@@ -7,9 +7,14 @@ class Command
                 respond("La liste n'a pas encore commencer !\nElle débute le Lundi a Midi (12:00 CET)")
                 return 
             end
-            if gamectl.in_list_or_waiting_list?(requester_username) or gamectl.pending_player?(requester_username) 
+            if gamectl.in_list_or_waiting_list?(requester_username)
                 log_info(gamectl.inspect)
                 respond("#{requester_fullname} est déja dans la liste! ❌")
+
+            elsif gamectl.pending_player?(requester_username) 
+                gamectl.confirm_waiting_player(playerctl.existing_player(requester_username))
+                respond("#{gamectl.get_last_player_in_list.fullname} rajouté a la liste des titulaires avec succes ✅ ⚽")
+                
             else
                 player = playerctl.get_player(requester_fullname,requester_username)
                 gamectl.add_player(player)
--- commands/group_chat/confirm_player_to_main_list.rb
+++ commands/group_chat/confirm_player_to_main_list.rb
@@ -5,7 +5,7 @@ class Command
         def run
             if gamectl.pending_player?(requester_username)
                 gamectl.confirm_waiting_player(playerctl.existing_player(requester_username))
-                respond("#{gamectl.get_last_player_in_list.fullname} rajouté a la liste avec succes ✅ ⚽")
+                respond("#{gamectl.get_last_player_in_list.fullname} rajouté a la liste des titulaires avec succes ✅ ⚽")
             else
                 respond("#{requester_fullname}, Vous n'etes pas en attente de confirmation ! ❌")
             end
